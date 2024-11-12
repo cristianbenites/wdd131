6674aca1913dd39a4f73d08d6e699740411c4f07
cristianbenites <cristian.jbenites@gmail.com>
2024-11-11 21:06:01 -0400

    week02 - Event Handling

---
+++ week02/scripts/bom.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+function addNewItem() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+    }
+    input.focus();
+}
+
+button.addEventListener('click', addNewItem);
+
+input.addEventListener('keyup', function(e) {
+    if (e.key === 'Enter') {
+        addNewItem();
+    }
+});
