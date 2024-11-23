daf1543d9c16d0019c5c44a29e508a03ff283339
cristianbenites <cristian.jbenites@gmail.com>
2024-11-23 13:00:27 -0400

    w03 assingment: place

--- scripts/place.js
+++ scripts/place.js
@@ -8,4 +8,26 @@ function getDates() {
     lastModElmt.innerHTML = `Last Modification: ${document.lastModified}`;
 }
 
+function getWindChill() {
+    const temperature = 10;
+    const windSpeed = 5;
+    const windChillElmt = document.getElementById("wind-chill");
+    let windChill = "N/A";
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        windChill = calculateWindChill(temperature, windSpeed) + "°C";
+    }
+
+    windChillElmt.textContent = windChill;
+}
+
+function calculateWindChill(temperature, windSpeed) {
+    const windChill = 13.12
+        + 0.6215 * temperature
+        - 11.37 * Math.pow(windSpeed, 0.16)
+        + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    return windChill.toFixed(2);
+}
+
+getWindChill();
 getDates();
